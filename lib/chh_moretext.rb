2ac33082b0baef9c82a9a458b3063a8b583a24e5
CHH <chh@chh.tw>
2013-01-04 16:07:11 +0800

    finished 0.1.0

--- lib/chh_moretext.rb
+++ lib/chh_moretext.rb
@@ -1,5 +1,30 @@
 require "chh_moretext/version"
+require "json"
+require "open-uri"
 
 module ChhMoretext
-  # Your code goes here...
+  class Base
+    class << self
+      def fetch_moretext(number, limit)
+        number = "n=#{number}"
+
+        if limit.is_a?(Range)
+          limit = "limit=#{limit.min},#{limit.max}"
+        elsif limit.is_a?(Integer)
+          limit = "limit=#{limit}"
+        else
+          limit = nil
+        end
+
+        if limit.nil?
+          condition = "?#{number}"
+        else
+          condition = "?#{number}&#{limit}"
+        end
+        return JSON(open("http://more.handlino.com/sentences.json#{condition}").read)["sentences"]
+      end
+    end
+  end
 end
+
+require "chh_moretext/moretext"
---
+++ lib/chh_moretext/moretext.rb
@@ -0,0 +1,23 @@
+module ChhMoretext
+  class Moretext < Base
+    class << self
+      def sentence(sentence_length_range = nil)
+        fetch_moretext(1, sentence_length_range)
+      end
+
+      def sentences(sentence_count = 1, sentence_length_range = nil)
+        fetch_moretext(sentence_count, sentence_length_range).join("")
+      end
+
+      def paragraph(sentence_count = 3)
+        sentences(sentence_count)
+      end
+
+      def paragraphs(paragraph_count = 3)
+        [].tap { |paragraphs|
+          paragraph_count.times { paragraphs << paragraph }
+          }.join("\r\n\r\n")
+      end
+    end
+  end
+end
